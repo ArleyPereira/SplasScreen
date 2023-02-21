93178537654ea80ece04387a710243976727a491
Arley Santana <arley1995@ucl.br>
2023-02-21 09:56:00 -0300

    first commit

---
+++ app/src/main/java/br/com/hellodev/splashtutorial/MainActivity.kt
@@ -0,0 +1,22 @@
+package br.com.hellodev.splashtutorial
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+
+class MainActivity : AppCompatActivity() {
+
+    private val viewModel: MainViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        installSplashScreen().apply {
+            setKeepOnScreenCondition {
+                viewModel.isLoading.value
+            }
+        }
+        setContentView(R.layout.activity_main)
+    }
+
+}
---
+++ app/src/main/java/br/com/hellodev/splashtutorial/MainViewModel.kt
@@ -0,0 +1,26 @@
+package br.com.hellodev.splashtutorial
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+
+class MainViewModel : ViewModel() {
+
+    private val _isLoading = MutableStateFlow(true)
+    val isLoading = _isLoading.asStateFlow()
+
+    init {
+        getData()
+    }
+
+    private fun getData() {
+        viewModelScope.launch {
+            delay(3000)
+            _isLoading.value = false
+        }
+    }
+
+}
